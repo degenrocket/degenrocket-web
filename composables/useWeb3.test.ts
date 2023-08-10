5119a61154736fd602513283e8bc065ec1f4e173
degenrocket <noreply@degenrocket.space>
2023-08-10 08:10:00 -0400

    test: add tests to web3

--- composables/useWeb3.test.ts
+++ composables/useWeb3.test.ts
@@ -19,6 +19,54 @@ describe('hideWeb3Modal', () => {
   });
 });
 
+// describe('connectWeb3Authenticator', () => {
+//   const { connectWeb3Authenticator } = useWeb3();
+
+  // test('connectWeb3Authenticator calls window.ethereum.request', async () => {
+  //   const mockRequest = vi.fn(() => Promise.resolve('value'));
+  //   vi.mock('window', () => ({ ethereum: { request: mockRequest } }));
+  //   const result = await connectWeb3Authenticator();
+  //   expect(mockRequest).toHaveBeenCalled();
+  //   expect(result).toEqual('value');
+  // });
+
+// test('connectweb3authenticator calls window.ethereum.request', async () => {
+//   const mockRequest = vi.fn(() => Promise.resolve('value'));
+//   // global.window = { ethereum: {} };
+//   window.ethereum = { request: mockRequest };
+//   await connectWeb3Authenticator();
+//   expect(mockRequest).toHaveBeenCalled();
+// });
+
+  // const { connectedAddress, connectWeb3Authenticator, pendingAuthentication, listAccounts, mockResolvedValueOnce } = useWeb3();
+  // test('should set pendingAuthentication to true', async () => {
+  //   await connectWeb3Authenticator();
+  //   expect(pendingAuthentication.value).toBe(true);
+  // });
+  //
+  // test('should set connectedAddress when accounts are available', async () => {
+  //   const mockAccounts = [{ address: '0x123' }];
+  //   listAccounts.mockResolvedValueOnce(mockAccounts);
+  //   await connectWeb3Authenticator();
+  //   expect(pendingAuthentication.value).toBe(false);
+  //   expect(connectedAddress.value).toBe(mockAccounts[0].address);
+  // });
+  //
+  // test('should set pendingAuthentication to false when no accounts are available', async () => {
+  //   listAccounts.mockResolvedValueOnce([]);
+  //   await connectWeb3Authenticator();
+  //   expect(pendingAuthentication.value).toBe(false);
+  // });
+  //
+  // test('should catch errors and set pendingAuthentication to false', async () => {
+  //   const mockError = new Error('Test error');
+  //   listAccounts.mockRejectedValueOnce(mockError);
+  //   await connectWeb3Authenticator();
+  //   expect(pendingAuthentication.value).toBe(false);
+  //   expect(console.error).toHaveBeenCalledWith(mockError);
+  // });
+// });
+
 describe('detectProvider', () => {
   const { detectProvider } = useWeb3();
   it('returns true if window.ethereum exists', async () => {
@@ -35,6 +83,15 @@ describe('detectProvider', () => {
     delete global.window;
   });
 });
+
+// describe('setRandomSigner', () => {
+//   const { setRandomSigner, signer } = useWeb3();
+//   it('should set the connected address to the signer address', () => {
+//     setRandomSigner();
+//     expect(signer.address).toBeDefined();
+//     expect(signer.address).toMatch(ethers.utils.getAddress(signer.address));
+//   });
+// });
 
 // sliceAddress
 describe('sliceAddress-each', () => {
