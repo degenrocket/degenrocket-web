d0e0d54cef52aa7c62e2a52c579743b0627aecaa
degenrocket <noreply@degenrocket.space>
2023-09-11 04:21:13 +0100

    fix: change environment variables in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
-    title: process.env.CUSTOM_META_TITLE,
+    title: process.env.META_TITLE,
       meta: [
         // 'hid:' key-value pair can be removed
-        { name: 'description', content: process.env.CUSTOM_META_DESCRIPTION },
-        { name: 'apple-mobile-web-app-title', content: process.env.CUSTOM_META_TITLE },
-        { name: 'og:title', property: 'og:title', content: process.env.CUSTOM_META_TITLE },
-        { name: 'og:site_name', property: 'og:site_name', content: process.env.CUSTOM_META_TITLE },
-        { name: 'og:description', property: 'og:description', content: process.env.CUSTOM_META_DESCRIPTION }
+        { name: 'description', content: process.env.META_DESCRIPTION },
+        { name: 'apple-mobile-web-app-title', content: process.env.META_TITLE },
+        { name: 'og:title', property: 'og:title', content: process.env.META_TITLE },
+        { name: 'og:site_name', property: 'og:site_name', content: process.env.META_TITLE },
+        { name: 'og:description', property: 'og:description', content: process.env.META_DESCRIPTION }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -21,14 +21,47 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiURL: process.env.API_URL,
-      apiHOST: process.env.API_HOST,
-      apiPORT: process.env.API_PORT,
 
       // Default meta values are used on other pages
       // so it's easy to change values in just one place.
-      defaultMetaAppName: process.env.CUSTOM_META_APP_NAME,
-      defaultMetaTitle: process.env.CUSTOM_META_TITLE,
-      defaultMetaDescription: process.env.CUSTOM_META_DESCRIPTION,
+      defaultMetaAppName: process.env.META_APP_NAME,
+      defaultMetaTitle: process.env.META_TITLE,
+      defaultMetaDescription: process.env.META_DESCRIPTION,
+
+      introTitle: process.env.INTRO_TITLE,
+      introAbout: process.env.INTRO_ABOUT,
+      ifShowContactsInIntro: process.env.IF_SHOW_CONTACTS_IN_INTRO,
+      ifShowIntroTutorial: process.env.IF_SHOW_INTRO_TUTORIAL,
+      ifShowHomeLatestComments: process.env.IF_SHOW_HOME_LATEST_COMMENTS,
+
+      // Another website links:
+      anotherWebsiteLink: process.env.ANOTHER_WEBSITE_LINK,
+      ipfsLink: process.env.IPFS_LINK,
+      ipfsHttpGatewayLink: process.env.IPFS_HTTP_GATEWAY_LINK,
+
+      // Social media links
+      nostrLink: process.env.NOSTR_LINK,
+      lensLink: process.env.LENS_LINK,
+      hiveLink: process.env.HIVE_LINK,
+      mirrorLink: process.env.MIRROR_LINK,
+      discordLink: process.env.DISCORD_LINK,
+      matrixLink: process.env.MATRIX_LINK,
+      telegramLink: process.env.TELEGRAM_LINK,
+      twitterLink: process.env.TWITTER_LINK,
+      redditLink: process.env.REDDIT_LINK,
+      youtubeLink: process.env.YOUTUBE_LINK,
+      githubLink: process.env.GITHUB_LINK,
+
+      // Blockchain
+      uniswapLink: process.env.UNISWAP_LINK,
+      sushiswapLink: process.env.SUSHISWAP_LINK,
+      etherscanLink: process.env.ETHERSCAN_LINK,
+
+      // Messengers
+      sessionName: process.env.SESSION_NAME,
+      telegramName: process.env.TELEGRAM_NAME,
+
+      ifShowDevelopersInfo: process.env.IF_SHOW_DEVELOPERS_INFO,
 
       ifShowCategoriesFilter: process.env.IF_SHOW_CATEGORIES_FILTER,
       ifAllowGuestLogin: process.env.IF_ALLOW_GUEST_LOGIN,
@@ -62,11 +95,11 @@ export default defineNuxtConfig({
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
-      name: process.env.CUSTOM_MANIFEST_NAME,
-      short_name: process.env.CUSTOM_MANIFEST_SHORT_NAME,
+      name: process.env.MANIFEST_NAME,
+      short_name: process.env.MANIFEST_SHORT_NAME,
       background_color: '#000000',
       theme_color: '#000000',
-      id: process.env.CUSTOM_MANIFEST_ID,
+      id: process.env.MANIFEST_ID,
       start_url: '.',
       display: 'standalone',
       lang: 'en',
