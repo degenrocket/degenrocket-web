06fa919f9cdbe8f010a4698706728b75114692a3
degenrocket <noreply@degenrocket.space>
2023-09-14 14:15:39 +0100

    feat: set persistent custom colors in .env

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,62 +5,64 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // You can customize these colors
+        // Do not change this file because it will be overwritten
+        // after you update the code e.g. using 'git pull'.
+        // You can customize these colors in the '.env' file.
         colorBase: {
-          light: '#000',
-          dark: '#d8d8d8'
+          light: process.env.COLOR_BASE_LIGHT || '#000000',
+          dark: process.env.COLOR_BASE_DARK || '#d8d8d8'
         },
         colorPrimary: {
-          light: '#ba14bf',
-          dark: '#ba14bf'
+          light: process.env.COLOR_PRIMARY_LIGHT || '#ba14bf',
+          dark: process.env.COLOR_PRIMARY_DARK || '#ba14bf'
         },
         colorSecondary: {
-          light: '#000000',
-          dark: '#ffffff'
+          light: process.env.COLOR_SECONDARY_LIGHT || '#000000',
+          dark: process.env.COLOR_SECONDARY_DARK || '#ffffff'
         },
         colorHover: {
-          light: '#243746',
-          dark: '#cbd4d1'
+          light: process.env.COLOR_HOVER_LIGHT || '#243746',
+          dark: process.env.COLOR_HOVER_DARK || '#cbd4d1'
         },
         colorNotImportant: {
-          light: '#9b8bc6',
-          dark: '#706297'
+          light: process.env.COLOR_NOT_IMPORTANT_LIGHT || '#9b8bc6',
+          dark: process.env.COLOR_NOT_IMPORTANT_DARK || '#706297'
         },
         colorGreen: {
-          light: '#089703',
-          dark: '#0ad203'
+          light: process.env.COLOR_GREEN_LIGHT || '#089703',
+          dark: process.env.COLOR_GREEN_DARK || '#0ad203'
         },
         colorRed: {
-          light: '#ff0a0a',
-          dark: '#ff0a0a'
+          light: process.env.COLOR_RED_LIGHT || '#ff0a0a',
+          dark: process.env.COLOR_RED_DARK || '#ff0a0a'
         },
         colorOrange: {
-          light: '#f4af0c',
-          dark: '#f4af0c'
+          light: process.env.COLOR_ORANGE_LIGHT || '#f4af0c',
+          dark: process.env.COLOR_ORANGE_DARK || '#f4af0c'
         },
         colorBlue: {
-          light: '#3a3dff',
-          dark: '#3a3dff'
+          light: process.env.COLOR_BLUE_LIGHT || '#3a3dff',
+          dark: process.env.COLOR_BLUE_DARK || '#3a3dff'
         },
         bgBase: {
-          light: '#fafafa',
-          dark: '#0F0F0F'
+          light: process.env.BG_BASE_LIGHT || '#fafafa',
+          dark: process.env.BG_BASE_DARK || '#0F0F0F'
         },
         bgSecondary: {
-          light: '#f0f0f0',
-          dark: '#181818'
+          light: process.env.BG_SECONDARY_LIGHT || '#f0f0f0',
+          dark: process.env.BG_SECONDARY_DARK || '#181818'
         },
         bgHover: {
-          light: '#e3e3e3',
-          dark: '#1f1f1f'
+          light: process.env.BG_HOVER_LIGHT || '#e3e3e3',
+          dark: process.env.BG_HOVER_DARK || '#1f1f1f'
         },
         bgDark: {
-          light: '#bbbbbb',
-          dark: '#000000'
+          light: process.env.BG_DARK_LIGHT || '#bbbbbb',
+          dark: process.env.BG_DARK_DARK || '#000000'
         },
         borderColor: {
-          light: '#ddd',
-          dark: '#312d3e'
+          light: process.env.BORDER_COLOR_LIGHT || '#ddd',
+          dark: process.env.BORDER_COLOR_DARK || '#312d3e'
         },
       }
     },
