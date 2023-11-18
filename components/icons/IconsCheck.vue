b1e1a20313360589cc6e4c5cd772f07037be13b8
degenrocket <noreply@degenrocket.space>
2023-11-18 01:14:28 +0000

    feat: add icons for copy, check, QR code, external link

---
+++ components/icons/IconsCheck.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Icon: Check -->
+  <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.5 12.75l6 6 9-13.5" stroke-linecap="round" stroke-linejoin="round"></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
