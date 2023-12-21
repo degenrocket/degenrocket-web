ded015146d4bbe1a50bcb0def406f07c81010330
degenrocket <noreply@degenrocket.space>
2023-12-21 21:49:15 +0100

    chore: add _error.vue to catch all non-error routes

---
+++ components/extra/ExtraPageNotFound.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="text-lg w-screen h-screen mx-auto text-center bg-bgBase-light dark:bg-bgBase-dark text-colorBase-light dark:text-colorBase-dark"
+  >
+    <p 
+      v-if="error && error.statusCode"
+      class="pt-7 text-7xl font-bold"
+    >{{ error.statusCode }}</p>
+    <p class="pt-7 text-md">starting the engine...</p>
+    <p class="text-md">no fuel</p>
+    <p class="text-md">error</p>
+    <p class="text-md">error</p>
+    <p class="text-md">error</p>
+    <p
+      v-if="error && error.message"
+      class="pt-7"
+    >{{ error.message }}</p>
+    <div
+      class="px-2 py-4 mx-auto mt-6 mb-12 max-w-sm cursor-pointer border-2 rounded-lg border-colorPrimary-light dark:border-colorPrimary-dark text-colorPrimary-light dark:text-colorPrimary-dark hover:bg-bgHover-light dark:hover:bg-bgHover-dark"
+      @click="handleClearError">
+      <IconsArrowLeft class="custom-icons" />
+      Try another rocket
+      <IconsRocket class="custom-icons" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  error?: any
+}>()
+
+const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/_error.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+// 404 errors are usually handled by ./error.vue, but
+// ./pages/_error.vue should catch all routes that didn't
+// match any other route and didn't throw any errors.
+const {randomNumber} = useWeb3()
+</script>
+
+<template>
+  <div>
+    <ExtraPageNotFound
+      :key="randomNumber()"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
