d779c3f4142df53bdd8e7401c36ccd80ce4b23dd
degenrocket <noreply@degenrocket.space>
2025-04-11 01:07:06 +0100

    pm2: add more env vars to config

--- ecosystem-prod.config.js
+++ ecosystem-prod.config.js
@@ -5,13 +5,13 @@ module.exports = {
   apps: [
     {
       // name: `prod-front@${packageJson.version}`,
-      name: 'dr-prod-front',
+      name: process.env.FRONTEND_PM2_PROD_NAME || 'dr-prod-front',
       script: './.output/server/index.mjs',
       exec_mode: process.env.FRONTEND_PM2_PROD_EXEC_MODE || 'cluster',
       instances: process.env.FRONTEND_PM2_PROD_INSTANCES || 'max',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: process.env.FRONTEND_PM2_PROD_MAX_MEMORY_RESTART || '1G',
       env: {
         NODE_ENV: "production",
         NITRO_PORT: parseInt(process.env.FRONTEND_PROD_PORT) || 3000,
--- ecosystem-stage.config.js
+++ ecosystem-stage.config.js
@@ -5,13 +5,13 @@ module.exports = {
   apps: [
     {
       // name: `stage-front@${packageJson.version}`,
-      name: 'dr-stage-front',
+      name: process.env.FRONTEND_PM2_STAGE_NAME || 'dr-stage-front',
       script: './.output/server/index.mjs',
       exec_mode: process.env.FRONTEND_PM2_STAGE_EXEC_MODE || 'cluster',
       instances: process.env.FRONTEND_PM2_STAGE_INSTANCES || '1',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: process.env.FRONTEND_PM2_STAGE_MAX_MEMORY_RESTART || '256M',
       env: {
         NODE_ENV: "staging",
         NITRO_PORT: parseInt(process.env.FRONTEND_STAGE_PORT) || 3000,
